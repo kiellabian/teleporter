c1ff12c37b49c9551e2c3365f1cf2f62c646e22c
Kiel Labian <kiellabian@gmail.com>
2016-06-30 20:02:53 +0800

    randomized starting point

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 var google = google;
-var latitude = -33.867386;
-var longitude = 151.195767;
+var streetviewService;
+var latitude = 10.3433083;
+var longitude = 123.9204285;
 
 function init() {
-    var streetviewService = new google.maps.StreetViewService;
-    var location = { location: { lat: latitude, lng: longitude } };
+    streetviewService = new google.maps.StreetViewService;
+    getRandomPlaceWithPanorama();
+}
+
+function getRandomPlaceWithPanorama() {
+    randomizeLatLong();
+    var location = {
+        location: { lat: latitude, lng: longitude },
+        preference: google.maps.StreetViewPreference.BEST
+    };
     streetviewService.getPanorama(
         location,
         function(result, status) {
+            console.log(latitude, longitude, status);
             if (status === google.maps.StreetViewStatus.OK) {
                 createPanorama(result);
+            } else {
+                getRandomPlaceWithPanorama();
             }
         });
 }
 
+function randomizeLatLong() {
+    latitude = getRandomInRange(-90, 90);
+    longitude = getRandomInRange(-180, 180);
+}
+
+
+function getRandomInRange(from, to) {
+    return (from + Math.random() * (to - from)).toFixed(7) * 1;
+}
+
 
 function createPanorama(result) {
-    var data = { pano: result.location.pano };
+    var data = {
+        pano: result.location.pano
+    };
     var panorama = new google.maps.StreetViewPanorama(
         document.getElementById('view'), data);
 }
